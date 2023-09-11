36f3ce7f5f4feede78f6278c2dffa9a699bccdb1
tangxl <1092678514@qq.com>
2023-09-11 18:35:47 +0800

    散列表

---
+++ src/dataStructures/HashTable.js
@@ -0,0 +1,89 @@
+/* 
+* 散列表
+ */
+
+import { defaultToString } from '../util';
+import { ValuePair } from './models/value-pair';
+
+export default class HashTable {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+  // 散列函数
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+  /* djb2HashCode(key) {
+    const tableKey = this.toStrFn(key);
+    let hash = 5381;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash = (hash * 33) + tableKey.charCodeAt(i);
+    }
+    return hash % 1013;
+  } */
+  // 散列函数
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+    // 向散列表增加一个新的项
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      this.table[position] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+    //根据键值检索值
+  get(key) {
+    const valuePair = this.table[this.hashCode(key)];
+    return valuePair == null ? undefined : valuePair.value;
+  }
+    // 根据键值从散列表移除值
+  remove(key) {
+    const hash = this.hashCode(key);
+    const valuePair = this.table[hash];
+    if (valuePair != null) {
+      delete this.table[hash];
+      return true;
+    }
+    return false;
+  }
+  // 获取全部散列表
+  getTable() {
+    return this.table;
+  }
+  // 判断散列表是否为空
+  isEmpty() {
+    return this.size() === 0;
+  }
+  // 散列表的大小
+  size() {
+    return Object.keys(this.table).length;
+  }
+  // 清除散列表
+  clear() {
+    this.table = {};
+  }
+  // 字符串化
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const keys = Object.keys(this.table);
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
+    for (let i = 1; i < keys.length; i++) {
+      objString = `${objString},{${keys[i]} => ${this.table[keys[i]].toString()}}`;
+    }
+    return objString;
+  }
+}
---
+++ src/dataStructures/hashTableSeparateChaining.js
@@ -0,0 +1,110 @@
+/* 
+* 散列表-分离链接
+ */
+
+import { defaultToString } from '../util';
+import LinkedList from './linkedList';
+import { ValuePair } from './models/value-pair';
+
+export default class HashTableSeparateChaining {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+  // 散列函数
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37;
+  }
+  // 散列函数
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+  // 向散列表增加一个新的项
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      if (this.table[position] == null) {
+        this.table[position] = new LinkedList();
+      }
+      this.table[position].push(new ValuePair(key, value));
+      return true;
+    }
+    return false;
+  }
+  //根据键值检索值
+  get(key) {
+    const position = this.hashCode(key);
+    const linkedList = this.table[position];
+    if (linkedList != null && !linkedList.isEmpty()) {
+      let current = linkedList.getHead();
+      while (current != null) {
+        if (current.element.key === key) {
+          return current.element.value;
+        }
+        current = current.next;
+      }
+    }
+    return undefined;
+  }
+  // 根据键值从散列表移除值
+  remove(key) {
+    const position = this.hashCode(key);
+    const linkedList = this.table[position];
+    if (linkedList != null && !linkedList.isEmpty()) {
+      let current = linkedList.getHead();
+      while (current != null) {
+        if (current.element.key === key) {
+          linkedList.remove(current.element);
+          if (linkedList.isEmpty()) {
+            delete this.table[position];
+          }
+          return true;
+        }
+        current = current.next;
+      }
+    }
+    return false;
+  }
+  // 判断是否为空
+  isEmpty() {
+    return this.size() === 0;
+  }
+  // 判断散列表大小
+  size() {
+    let count = 0;
+    Object.values(this.table).forEach(linkedList => {
+      count += linkedList.size();
+    });
+    return count;
+  }
+  // 清除散列表
+  clear() {
+    this.table = {};
+  }
+   // 获取全部散列表
+  getTable() {
+    return this.table;
+  }
+  // 字符串化
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const keys = Object.keys(this.table);
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
+    for (let i = 1; i < keys.length; i++) {
+      objString = `${objString},{${keys[i]} => ${this.table[
+        keys[i]
+      ].toString()}}`;
+    }
+    return objString;
+  }
+}
