26024232b15a5952cf2c89728f1939c641af5718
tangxl <1092678514@qq.com>
2023-10-20 09:49:51 +0800

    二叉搜索树

--- src/util/index.js
+++ src/util/index.js
@@ -1,19 +1,30 @@
-export function defaultEquals(a, b) {
-    return a === b;
-  }
-
 export const Compare = {
-    LESS_THAN: -1,
-    BIGGER_THAN: 1,
-    EQUALS: 0
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
 };
 
+export const DOES_NOT_EXIST = -1;
+
+export function lesserEquals(a, b, compareFn) {
+  const comp = compareFn(a, b);
+  return comp === Compare.LESS_THAN || comp === Compare.EQUALS;
+}
+
+export function biggerEquals(a, b, compareFn) {
+  const comp = compareFn(a, b);
+  return comp === Compare.BIGGER_THAN || comp === Compare.EQUALS;
+}
 
 export function defaultCompare(a, b) {
   if (a === b) {
     return Compare.EQUALS;
   }
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+export function defaultEquals(a, b) {
+  return a === b;
 }
 
 export function defaultToString(item) {
@@ -25,4 +36,18 @@ export function defaultToString(item) {
     return `${item}`;
   }
   return item.toString();
+}
+
+export function swap(array, a, b) {
+  /* const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp; */
+  [array[a], array[b]] = [array[b], array[a]];
+}
+export function reverseCompare(compareFn) {
+  return (a, b) => compareFn(b, a);
+}
+
+export function defaultDiff(a, b) {
+  return Number(a) - Number(b);
 }
