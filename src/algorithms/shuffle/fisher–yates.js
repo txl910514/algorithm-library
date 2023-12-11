f684d9245a254dce3f5f72d270e4da8683ee407b
tangxl <1092678514@qq.com>
2023-12-11 09:53:59 +0800

    随机算法

---
+++ src/algorithms/shuffle/fisher–yates.js
@@ -0,0 +1,12 @@
+// 随机算法
+import { swap } from '../../util';
+
+export function shuffle(array) {
+  let currentIndex = array.length;
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    swap(array, currentIndex, randomIndex);
+  }
+  return array;
+}
